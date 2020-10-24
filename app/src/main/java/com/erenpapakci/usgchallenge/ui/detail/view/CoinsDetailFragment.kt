1ea70ce4cc92d46bb77b5d125419c0b8e330af3d
Eren Papakçı <erenpapakci@gmail.com>
2020-10-24 20:59:14 +0300

    detail ui desing it

--- app/src/main/java/com/erenpapakci/usgchallenge/ui/detail/view/CoinsDetailFragment.kt
+++ app/src/main/java/com/erenpapakci/usgchallenge/ui/detail/view/CoinsDetailFragment.kt
@@ -1,14 +1,20 @@
 package com.erenpapakci.usgchallenge.ui.detail.view
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import com.erenpapakci.usgchallenge.R
 import com.erenpapakci.usgchallenge.base.BaseViewModelFragment
+import com.erenpapakci.usgchallenge.base.extensions.currencyFormatter
+import com.erenpapakci.usgchallenge.base.extensions.loadImage
 import com.erenpapakci.usgchallenge.data.DataHolder
 import com.erenpapakci.usgchallenge.data.Status
 import com.erenpapakci.usgchallenge.data.remote.model.Coins
 import com.erenpapakci.usgchallenge.ui.dashboard.viewmodel.CoinsViewModel
 import com.erenpapakci.usgchallenge.ui.detail.viewmodel.CoinsDetailViewModel
+import com.yabu.livechart.model.DataPoint
+import com.yabu.livechart.model.Dataset
+import com.yabu.livechart.view.LiveChartStyle
 import kotlinx.android.synthetic.main.fragment_detail_coin.*
 
 class CoinsDetailFragment: BaseViewModelFragment<CoinsDetailViewModel>() {
@@ -42,7 +48,57 @@ class CoinsDetailFragment: BaseViewModelFragment<CoinsDetailViewModel>() {
     }
 
     private fun showUI(coin: Coins?) {
-        coin?.name
+        coin?.name?.let { name ->
+            textViewCoinName.text = name
+            textViewStatisticTitle.text =
+                """$name ${resources.getString(R.string.statistics_title)}"""
+        }
+
+        coin?.iconUrl.let {
+            imageViewIcon.loadImage(it)
+        }
+
+        coin?.history.let { history ->
+            val dataPoint = mutableListOf<DataPoint>()
+            var xValues  = 0F
+            history.let { historyList ->
+                historyList?.forEach { history ->
+                    dataPoint.add(DataPoint(xValues,history.toFloat()))
+                    xValues += 1
+                }
+            }
+            val historyData = dataPoint?.let { Dataset(it) }
+
+            val style = LiveChartStyle().apply {
+                mainColor = R.color.colorRed
+                mainFillColor = Color.MAGENTA
+                baselineColor = Color.BLUE
+                pathStrokeWidth = 12f
+                baselineStrokeWidth = 6f
+            }
+
+            liveChart.setDataset(historyData)
+                .drawSmoothPath()
+                .setLiveChartStyle(style)
+                .drawDataset()
+        }
+
+        coin?.price.let { price ->
+            textViewStatisticPriceToUsd.text = """${price?.currencyFormatter(price)} $"""
+            textViewStatisticPriceToBtc.text = """${price?.currencyFormatter(price)} $"""
+        }
+
+        coin?.rank.let { rank ->
+            textViewStatisticRank.text = rank.toString()
+        }
+
+        coin?.volume.let { volume ->
+            textViewStatistic24hVolume.text = """${volume?.currencyFormatter(volume)} $"""
+        }
+
+        coin?.marketCap.let { marketCap ->
+            textViewStatisticMarketCap.text = """${marketCap?.currencyFormatter(marketCap)} $"""
+        }
     }
 
     companion object {
