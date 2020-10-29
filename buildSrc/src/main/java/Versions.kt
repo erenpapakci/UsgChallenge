532bf637aac787ce32a23fa113455fc1cf4df727
Eren Papakçı <erenpapakci@gmail.com>
2020-10-29 15:00:21 +0300

    buildSrc addedd

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,35 @@
+/*
+ * Sdk and dependency versions
+ */
+object Versions {
+    /*
+     * Sdk Versions
+     */
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val compileSdkVersion = 30
+    const val buildToolVersion =  "30.0.2"
+
+    /*
+     * Dependency Versions
+     */
+    const val xVersion = "1.0.0"
+    const val gradleVersion = "4.1.0"
+    const val kotlinVersion = "1.4.10"
+    const val jUnitVersion = "4.12"
+    const val testRunnerVersion = "1.1.0"
+    const val espressoCoreVersion = "3.1.0"
+    const val dagger2Version = "2.16"
+    const val javaxAnnotationVersion = "10.0-b28"
+    const val retrofitVersion = "2.5.0"
+    const val okHttpLoggingInterceptorVersion = "3.8.0"
+    const val gsonVersion = "2.0.2"
+    const val okHttpVersion = "3.10.0"
+    const val viewModelVersion = "2.0.0"
+    const val rxKotlinVersion = "2.3.0"
+    const val rxAndroidVersion = "2.1.1"
+    const val picassoVersion = "2.71828"
+    const val mockitoVersion = "2.18.3"
+    const val supportDesignVersion = "1.0.0-rc01"
+    const val scalingLayoutVersion = "1.2.1"
+}
